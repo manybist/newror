1c8393384660488b3228983928b1441bdd45dba8
many <many.bist@gmail.com>
2015-08-17 21:51:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newror::Application.config.secret_token = 'cc00ad864f03ab153a097336cf85792702754f3c7771e112b842dfae4f2522caa6d7f5f0f6cd73a86f50ea4bd57c8addaac76c5cba6412de6fb15b902496c385'
